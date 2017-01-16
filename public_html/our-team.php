7d8f3e45ec1c35c09ceaca882907937600085f19
SirLeonardIV <leonard.ha@gmail.com>
2017-01-16 17:48:30 -0500

    changed team sizes

--- public_html/our-team.php
+++ public_html/our-team.php
@@ -7,10 +7,10 @@
 <!-- form -->
 
 <div class="row">
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Cindy Nie</h3><h4>President</h4><p>Description</p></div></div></div>
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Lucy Li</h3><h4>VP Finance</h4><p> Description</p></div></div></div>
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Sabrina Leung</h3><h4>VP Public Relations</h4><p> Description</p></div></div></div>
-  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Carmen Louie</h3><h4>IT Coordinator</h4><p> Description</p></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Cindy Nie</h3><h4>President</h4><p>Description</p></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Lucy Li</h3><h4>VP Finance</h4><p> Description</p></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Sabrina Leung</h3><h4>VP Public Relations</h4><p> Description</p></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Carmen Louie</h3><h4>IT Coordinator</h4><p> Description</p></div></div></div>
   <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Iris Zhang</h3><h4>Sponsorship Coordinator</h4><p> Description</p></div></div></div>
   <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Hannah Wei</h3><h4>Sponsorship Coordinator</h4><p> Description</p></div></div></div>
   <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/headshot-placeholder.jpg" class="img-responsive"><div class="info"><h3>Rupinderjit Cheema</h3><h4>Events Coordinator</h4><p> Description</p></div></div></div>
