0e37ded8fe836dbdfbad33fef5c2feccfbda289d
SirLeonardIV <leonard.ha@gmail.com>
2017-02-28 03:51:04 -0500

    Changed to a one pager

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,75 @@
-<?php include 'header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>QHTC | Queen's Hospitality and Tourism Club</title>
 
+<!-- Google fonts -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+<!-- uniform -->
+<link type="text/css" rel="stylesheet" href="assets/uniform/css/uniform.default.min.css" />
+
+<!-- animate.css -->
+<link rel="stylesheet" href="assets/wow/animate.css" />
+
+
+<!-- gallery -->
+<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+
+
+<!-- favicon -->
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+<link rel="stylesheet" href="assets/style.css">
+
+</head>
+
+<body id="home">
+
+
+<!-- top 
+  <form class="navbar-form navbar-left newsletter" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Enter Your Email Id Here">
+        </div>
+        <button type="submit" class="btn btn-inverse">Subscribe</button>
+    </form>
+ top -->
+
+<!-- header -->
+<nav class="navbar  navbar-default" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img src="images/QHTClogo.jpg"  alt="QHTC"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+      
+      <ul class="nav navbar-nav">        
+        <li><a class="#aboutbutton">About</a></li>        
+        <li><a href="our-team.php">Our Team</a></li>
+        <li><a href="sponsorship.php">Sponsorship & Partners</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div><!-- Wnavbar-collapse -->
+  </div><!-- container-fluid -->
+</nav>
+<!-- header -->
 
 
 <!-- banner -->
@@ -16,64 +86,220 @@
 </div>
 <!-- banner-->
 
-
 <!-- services -->
-<div class="spacer services">
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <!-- RoomCarousel -->
-            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                <div class="item active"><img src="images/photos/1.jpg" class="img-responsive" alt="slide"></div>                
-                <div class="item  height-full"><img src="images/photos/2.jpg"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/3.jpg"  class="img-responsive" alt="slide"></div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-            <!-- RoomCarousel-->
-            <div class="caption">Our Team<a href="our-team.php" class="pull-right"><i class="fa fa-external-link"></i></a></div>
-        </div>
+<div class="container about">
 
+      <div class="animated fadeInDown" id="whoWeAre">
+      <div class="text-center" style="margin-bottom:20px;">
+      	<img src="images/photos/Group_Picture.jpg">
+      </div>
+      <h3 style="font-family:'Droid Serif', serif;"><b>Who We Are</b></h3>
+      <p>
+      The Queen’s Hospitality and Tourism Club (QHTC) provides opportunities for Queen’s students to explore the thriving hospitality and tourism industry. We provide students with deeper insights into the industry at both the local Canadian level and international level.
+      </p>
+      <p>
+      As a club we are committed to developing an integrated communication platform between Queens students and industry professionals through various valuable networking opportunities and exciting events. There are many ways for students to get involved with our club, such as, joining us for alumni and speaker series events. In addition, we’ll be providing students the opportunity to gain first-hand experiences with industry professionals through local visits to hotels, restaurants, and other popular tourism destinations within the Kingston vicinity and extending outwards throughout Ontario.
+      </p>
+      <p><b>
+      QHTC welcomes students from diverse academic backgrounds and we strive to create a welcoming community amongst students. We’re always looking forward to meeting new faces from all faculties!
+      </b></p>
+      <br>
+      </div>
 
-        <div class="col-sm-4">
-            <!-- RoomCarousel -->
-            <div id="TourCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                <div class="item active"><img src="images/photos/4.jpg" class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/5.jpg"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/6.jpg"  class="img-responsive" alt="slide"></div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#TourCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="right carousel-control" href="#TourCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-            <!-- RoomCarousel-->
-            <div class="caption">Events<a href="events.php" class="pull-right"><i class="fa fa-external-link"></i></a></div>
-        </div>
+      <div class="animated fadeInDown" id="mandate">
+      <h3 style="font-family:'Droid Serif', serif;"><b>Mandate</b></h3>
+      <p>
+      Queen’s Hospitality and Tourism Club’s mandate is to provide a common platform for students to openly explore the rapidly growing industry of hospitality and tourism. We strive to broaden members’ interest and knowledge in this relatively unexplored industry by providing unique opportunities for members to gain hands-on experiences. As a club, we also dedicated to building connections between students and real-life industry professionals, providing valuable networking experiences to help enhance members’ ability to pursue a future career in the industry. 
+      </p>
+      <p>
+      Through the abundant exposure that members will gain in the industry we also aim to promote cultural diversity and communication among Queen’s students as well as Kingstonians. QHTC hopes to unite students from diverse academic backgrounds to create a welcoming community amongst students while engaging in exciting and fun events that will allow members to gain new knowledge and practical skills in the relatively unexplored industry of hospitality and tourism. 
+      </p>
+      </div>
 
+</div>
 
-        <div class="col-sm-4">
-            <!-- RoomCarousel -->
-            <div id="FoodCarousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                <div class="item active"><img src="images/photos/7.jpg" class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/8.jpg"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/9.jpg"  class="img-responsive" alt="slide"></div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#FoodCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="right carousel-control" href="#FoodCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-            <!-- RoomCarousel-->
-            <div class="caption">Contact Us<a href="contact.php" class="pull-right"><i class="fa fa-external-link"></i></a></div>
-        </div>
+<div class="container team">
+<div class="text-center">
+<h3 style="font-family:'Droid Serif', serif;font-size:25px;"><b>Our Team</b></h3>
+</div>
+<!-- form -->
+
+<div class="row">
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Cindy_Nie_PRESIDENT.jpg" class="img-responsive"><div class="info"><h3>Cindy Nie</h3><h4>President</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Lucy_Li_VPFINANCE.jpg" class="img-responsive"><div class="info"><h3>Lucy Li</h3><h4>VP Finance</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Sabrina_Leung_VP_PUBLIC_RELATIONS.jpg" class="img-responsive"><div class="info"><h3>Sabrina Leung</h3><h4>VP Public Relations</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Carmen_Louie_IT.jpg" class="img-responsive"><div class="info"><h3>Carmen Louie</h3><h4>IT Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Iris_Zhang_SPONS.jpg" class="img-responsive"><div class="info"><h3>Iris Zhang</h3><h4>Sponsorship Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Hannah_Wei_SPONS.jpg" class="img-responsive"><div class="info"><h3>Hannah Wei</h3><h4>Sponsorship Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Rupinderjit_Cheema_EVENTS.jpg" class="img-responsive"><div class="info"><h3>Rupinderjit Cheema</h3><h4>Events Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Karoline_Sarah_Wang_EVENTS.jpg" class="img-responsive"><div class="info"><h3>Karoline Sarah Wang</h3><h4>Events Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Claire_Currie_EVENTS.jpg" class="img-responsive"><div class="info"><h3>Claire Currie</h3><h4>Events Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Andy_Tran_MARKETING.jpg" class="img-responsive"><div class="info"><h3>Andy Tran</h3><h4>Marketing Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Yan_Wu_MARKETING.jpg" class="img-responsive"><div class="info"><h3>Yan Wu</h3><h4>Marketing Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Irene_Cen_MARKETING.jpg" class="img-responsive"><div class="info"><h3>Irene Cen</h3><h4>Marketing Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Christopher_Zhao_LOGS.jpg" class="img-responsive"><div class="info"><h3>Christopher Zhao</h3><h4>Logistics Coordinator</h4></div></div></div>
+  <div class="col-sm-4 wowload fadeInUp"><div class="rooms"><img src="images/photos/Evgeny_Kim_LOGS.jpg" class="img-responsive"><div class="info"><h3>Evgeny Kim</h3><h4>Logistics Coordinator</h4></div></div></div>
+ 
+</div>
+
+</div>
+</div>
+
+<div class="container sponsorship">
+<div class="text-center">
+<h1 class="title">Sponsorship</h1>
+</div>
+
+<div class="row gallery">
+			<div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture1.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture2.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture3.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture4.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture5.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture6.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture8.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture9.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture10.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture11.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture12.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture13.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture14.png" class="gallery-image img-responsive"></div>
+              <div class="col-sm-3 wowload fadeInUp"><img src="images/sponsorship-photos/Picture15.png" class="gallery-image img-responsive"></div>
+
+              
+       </div>
+
+       <br>
+       <br>
+</div> <!--Close container-->
+
+<div class="container contact">
+
+<h1 class="title">Contact Us</h1>
+
+	<div class="row">
+	<div class="col-lg-12 col-sm-12" style="font-family: 'Droid Serif', serif !important;font-size:1.15em;">
+		<p><b>Email us at: </b> hospitalitytourism@clubs.queensu.ca</p>
+		<p><b>Facebook: </b>https://www.facebook.com/QHTClub/</p>
+		<p><b>Twitter: </b>QueensHTC</p>
+		<p><b>Instagram: </b>qhtc_kingston</p>
+		<br>
+
+	</div>
     </div>
+
+    <div class="row">	
+    	<div class="col-sm-12">
+    		<div class="map">
+       			<iframe src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ0SeYswWr0kwR4UPQwONZl_U&key=AIzaSyDPAdwDeLv5frPF-7CNSe0yhtovG27_ex0"  width="100%" height="300" frameborder="0"></iframe>	
+       		</div>
+    	</div>
+	</div>
+<!-- form -->
+
 </div>
-</div>
-<!-- services -->
+
+<footer class="spacer">
+        <div class="container">
+            <div class="row">            
+                 
+                <div class="col-sm-3">
+                    <h4>Quick Links</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>        
+                        <li><a href="our-team.php">Our Team</a></li>
+                        <li><a href="events.php">Events</a></li>
+                        <li><a href="sponsorship.php">Sponsorship & Partners</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-sm-9">
+                    <h4>Queen's Hospitality and Tourism Club</h4>
+                    <p style="font-size:16px;">“Opening doors to the thriving hospitality & tourism industry.”</p>
+                </div>  
+            </div>
+            <!--/.row--> 
+        </div>
+        <!--/.container-->    
+    
+    <!--/.footer-bottom--> 
+</footer>
 
 
-<?php include 'footer.php';?>
+<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+    The container for the modal slides
+    <div class="slides"></div>
+    Controls for the borderless lightbox
+    <h3 class="title">title</h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    The modal dialog, which will be used to wrap the lightbox content 
+</div>-->
+
+
+<script src="assets/jquery.js"></script>
+
+<!-- wow script -->
+<script src="assets/wow/wow.min.js"></script>
+
+<!-- uniform -->
+<script src="assets/uniform/js/jquery.uniform.js"></script>
+
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="assets/mobile/touchSwipe.min.js"></script>
+
+<!-- jquery mobile -->
+<script src="assets/respond/respond.js"></script>
+
+<!-- gallery -->
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+
+<!-- custom script -->
+<script src="assets/script.js"></script>
+
+<script>
+$("#aboutbutton").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".about").offset().top
+    }, 2000);
+});
+
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#elementtoScrollToID").offset().top
+    }, 2000);
+});
+
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#elementtoScrollToID").offset().top
+    }, 2000);
+});
+
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#elementtoScrollToID").offset().top
+    }, 2000);
+});
+
+</script>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
